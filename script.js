5235389b6a0899d62b6619417192ef1ed85454fb
homei-leung <49667943+homei-leung@users.noreply.github.com>
2024-09-15 20:13:09 -0400

    add js to create 16x16 grid

---
+++ script.js
@@ -0,0 +1,36 @@
+const container = document.querySelector(".container");
+const button = document.createElement("button");
+const body = document.querySelector("body");
+button.textContent = "Generate grid";
+button.classList.add("button");
+body.insertBefore(button, container);
+
+// Generate a 16x16 grid
+let gridWidth = 16;
+const containerWidth = 800;
+let pixelWidth = containerWidth/gridWidth;
+let pixelHeight = pixelWidth;
+let totalPixels = gridWidth**2;
+
+for (let i = 0; i < totalPixels; i++){
+    const pixel = document.createElement("div");
+    pixel.style.width = `${pixelWidth}px`;
+    pixel.style.height = `${pixelHeight}px`;
+    pixel.classList.add("pixel");
+    pixel.classList.add("uncolored");
+    container.appendChild(pixel);
+}
+
+
+// Add event listener for when hover occurs over div, the color changes
+
+// Function to generate a new grid
+    // get user input from user alert
+    // continue to request user input if input is > 100
+    // assign the user input as the number of divs ( n x n grid)
+    // remove the old grid
+
+// Add event listener for when the Generate grid button is pressed, alert the user for input
+
+// Extra credit: the square's RGB values are randomized with each interaction
+// Extra credit: implement a progressive darkening effect where each interaction darkens the square by 10%
