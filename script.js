d9ed13cda2af80d9d6437cd7b447f94a4459f411
homei-leung <49667943+homei-leung@users.noreply.github.com>
2024-09-16 11:12:39 -0400

    create and call generateGrid function, add btn-container to center button

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const container = document.querySelector(".container");
 const button = document.createElement("button");
+const buttonContainer = document.createElement("div");
+buttonContainer.classList.add("btn-container");
 const body = document.querySelector("body");
 button.textContent = "Generate grid";
 button.classList.add("button");
-body.insertBefore(button, container);
+body.insertBefore(buttonContainer, container);
+buttonContainer.appendChild(button);
 
 // Generate a 16x16 grid
 let gridWidth = 16;
@@ -12,15 +15,18 @@ let pixelWidth = containerWidth/gridWidth;
 let pixelHeight = pixelWidth;
 let totalPixels = gridWidth**2;
 
-for (let i = 0; i < totalPixels; i++){
-    const pixel = document.createElement("div");
-    pixel.style.width = `${pixelWidth}px`;
-    pixel.style.height = `${pixelHeight}px`;
-    pixel.classList.add("pixel");
-    pixel.classList.add("uncolored");
-    container.appendChild(pixel);
+function generateGrid(){
+    for (let i = 0; i < totalPixels; i++){
+        const pixel = document.createElement("div");
+        pixel.style.width = `${pixelWidth}px`;
+        pixel.style.height = `${pixelHeight}px`;
+        pixel.classList.add("pixel");
+        pixel.classList.add("uncolored");
+        container.appendChild(pixel);
+    }
 }
 
+generateGrid();
 
 // Add event listener for when hover occurs over div, the color changes
 
